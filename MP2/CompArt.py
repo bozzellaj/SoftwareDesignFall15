0f89a86ba37f56f8ccf34961829c2d1ba6e34ab8
bozzellaj <jbozzella@students.olin.edu>
2015-12-11 14:10:26 -0500

    Program, 5 Examples and README

---
+++ MP2/CompArt.py
@@ -0,0 +1,90 @@
+'''
+ComputationalArt.py
+John Bozzella
+'''
+
+from random import choice
+import numpy.random as npr
+from math import *
+from PIL import Image
+
+def blockrecurse(depth, blocks, parameters):
+	if depth == 1:
+		return (choice(blocks), choice(parameters))
+	else:
+		depth = depth-1
+		return (choice(blocks), blockrecurse(depth, blocks, parameters))
+	
+def functioneval(function, a, b):
+	if function[0] == "x":
+		func = a
+	elif function[0] == "y":
+		func = b
+	elif function[0] == "prod(x, y)":
+		func = functioneval(function[1], a, b)*b
+	elif function[0] == "avg(x, y)":
+		func = (functioneval(function[1], a, b)+b)/2.0
+	elif function[0] == "cos_pi(x)":
+		func = cos(pi*functioneval(function[1], a ,b))
+	elif function[0] == "sin_pi(x)":
+		func = cos(pi*functioneval(function[1], a, b))
+	return func
+
+def remap_interval(val, inp_int_start, inp_int_end, out_int_start, out_int_end):
+	""" 
+	Maps the input value that is in the interval [input_interval_start, input_interval_end]
+	to the output interval [output_interval_start, output_interval_end].  The mapping
+	is an affine one (i.e. output = input*c + b).  Calculates the intervals, finds the relative position
+	in the input interval compared to the output and then remaps the that relative position in the output interval
+	"""
+
+	input_diff = float(inp_int_end) - float(inp_int_start)
+	output_diff = float(out_int_end) - float(out_int_start)
+	position = val - float(inp_int_start)
+	relative_position = position/input_diff
+	remap = (relative_position * output_diff) + out_int_start
+
+	return remap
+
+def colormap(val):
+	""" Assign remapped values a color """
+
+	color = remap_interval(val, -1, 1, 0, 255)
+
+	return int(color)
+
+def create(filename, width, height):
+	""" create the actual art and save as an image file.
+	file is a string that is the filename for the image (should be .png)
+	width and height are used set image dimensions
+	"""
+	# Define the necessary variables and parameters
+	min_depth = 6
+	max_depth = 10
+	depth =  npr.randint(min_depth, max_depth+1)
+	a = 2.0*npr.random()-1
+	b = 2.0*npr.random()-1
+	blocks = ["prod(x, y)" , "avg(x, y)", "cos_pi(x)", "sin_pi(x)", "x" , "y"]
+	parameters = ["x", "y"]
+
+	# Generate a function for each color
+	functionr = blockrecurse(depth, blocks, parameters)	
+	functiong = blockrecurse(depth, blocks, parameters)
+	functionb = blockrecurse(depth, blocks, parameters)
+    
+	# Generate image and loop color mapping on each pixel
+	picture = Image.new("RGB", (width, height))
+	pixels = picture.load()
+	for i in range(width):
+		for j in range(height):
+			x = remap_interval(i, 0, width, -1, 1)
+			y = remap_interval(j, 0, height, -1, 1)
+			pixels[i, j] = (
+                    colormap(functioneval(functionr, x, y)),
+                    colormap(functioneval(functiong, x, y)),
+                    colormap(functioneval(functionb, x, y))
+                    )
+
+	picture.save(filename)
+
+create('test5.png',500,500)
---
+++ MP2/README.txt
@@ -0,0 +1,5 @@
+John Bozzella
+SoftDes Fall 2015
+MP2
+
+CompArt.py: Creates computational art using randomly generated functions.  Set depth, (min_depth and max_depth, default  6 and 10 respectively,) to adjust the complexity of the functions.  Once depth has been set simply run the program to save a .png of the art.  
